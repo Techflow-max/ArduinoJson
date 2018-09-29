9029a21abded714b289e6abfc4e03fbc871e82da
Benoit Blanchon <github@benoitblanchon.fr>
2018-09-29 15:28:29 +0200

    Tranform JsonVariantData into a POD to please clang

--- src/ArduinoJson/MsgPack/MsgPackDeserializer.hpp
+++ src/ArduinoJson/MsgPack/MsgPackDeserializer.hpp
@@ -281,9 +281,9 @@ class MsgPackDeserializer {
       JsonVariant key(_memoryPool, &keyData);
       DeserializationError err = parse(key);
       if (err) return err;
-      if (!keyData.isString()) return DeserializationError::NotSupported;
+      if (!key.is<char *>()) return DeserializationError::NotSupported;
 
-      JsonVariant value = object.set(keyData.asString());
+      JsonVariant value = object.set(keyData.content.asString);
       if (value.isInvalid()) return DeserializationError::NoMemory;
 
       err = parse(value);
